a7cd69b35adc165ca377930199945bf2bdd04a49
luan <luanndm1@hdbank.com.vn>
2024-08-15 10:38:58 +0700

    fix issue 2

--- FirstProject/Controllers/HomeController.cs
+++ FirstProject/Controllers/HomeController.cs
@@ -15,17 +15,20 @@ namespace FirstProject.Controllers
 
         public IActionResult Index()
         {
+            _logger.LogInformation($"[GET][Home/Index]-[User: {User.Identity?.Name ?? "Unknow"}]");
             return View();
         }
 
         public IActionResult Privacy()
         {
+            _logger.LogInformation($"[GET][Home/Privacy]-[User: {User.Identity?.Name ?? "Unknow"}]");
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            _logger.LogError($"[Home]-[User: {User.Identity?.Name ?? "Unknow"}]");
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
